7e1f9b4ed93b4e7d7f3014a5911037f06d4eff86
crystal.manas@ftwfoundation.org <crystal.manas@ftwfoundation.org>
2026-09-14 08:40:50 +0000

    chore: add test notebook to verify source format

---
+++ notebooks/test_notebook_format_source.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
+print("this is a test notebook to verify that the format defaults to source, not ipynb, and that outputs are cleared")
+
+# COMMAND ----------
+
+y = 1 + 1
+print(y)
